c34b1496915388df6c2fad933bfb0fe93d64efe1
wr <1161609893@qq.com>
2017-02-06 13:55:16 +0800

    聊天主界面布局完成监听设置完成，下一步socket数据传输到后台
    @Jackson_run
    2017.2.6

--- src/main/java/com/wr/MainPanel.java
+++ src/main/java/com/wr/MainPanel.java
@@ -13,11 +13,9 @@ import java.awt.Font;
 import java.awt.Color;
 import java.awt.BorderLayout;
 
+import java.awt.event.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.text.*;
 
@@ -66,13 +64,52 @@ public class MainPanel extends JPanel{
 		final ImageIcon icon1=new ImageIcon("../Image/logoCola.JPG");
 		final ImageIcon icon2=new ImageIcon("../Image/Cola.JPG");
 		//ImageIcon icon3=new ImageIcon("../java/com/wr/RobotClient/image1/expression/Look_1.png");
-
+		text=new JTextPane();
 		send_btn=new JButton(icon1);
 		send_btn.setFont(new Font("楷体_gb2312",Font.BOLD,15));
 		send_btn.setText("发送");
 		send_btn.setHorizontalTextPosition(SwingConstants.CENTER);
 		send_btn.setContentAreaFilled(false);
 		send_btn.setBorder(null);
+		send_btn.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				String inputStr = text.getText();
+				SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				insertDocument("我  :"+sf.format(new Date())+"\n",Color.GREEN);
+				insertDocument(inputStr,Color.black);
+				text.setText("");
+			}
+
+			public void mousePressed(MouseEvent e) {
+			}
+
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			public void mouseExited(MouseEvent e) {
+			}
+		});
+		text.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {
+				if(e.getKeyChar()==KeyEvent.VK_ENTER) {
+					String inputStr = text.getText().trim();
+					SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					insertDocument("我  :" + sf.format(new Date()) + "\n", Color.GREEN);
+					insertDocument(inputStr, Color.black);
+					text.setText("");
+				}
+			}
+
+			public void keyPressed(KeyEvent e) {
+			}
+
+			public void keyReleased(KeyEvent e) {
+			}
+		});
+
 	}
 	public void initB()//设置右边部分
 	{
@@ -93,7 +130,7 @@ public class MainPanel extends JPanel{
 	public void initC()//设置文本框部分
 	{
 		pane=new JTextPane();
-		text=new JTextPane();
+
 		pane.setFont(new Font("宋体",Font.PLAIN,15));
 		scroll=new JScrollPane(text,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -104,7 +141,8 @@ public class MainPanel extends JPanel{
 
 		SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		insertDocument("球球:"+sf.format(new Date())+"\n",Color.BLUE);
-		insertDocument("  hi,我是机器人球球,很高兴认识你",Color.black);
+		insertDocument("  hi,我是机器人球球,很高兴认识你\n",Color.black);
+		//insertDocument("test",Color.BLUE);
 	}
 	public void setSplitPane()
 	{
@@ -130,8 +168,8 @@ public class MainPanel extends JPanel{
 		SimpleAttributeSet set = new SimpleAttributeSet();
 		StyleConstants.setForeground(set, textColor);//设置文字颜色
 		StyleConstants.setFontSize(set, 15);//设置字体大小
-		Document doc = pane.getStyledDocument();
-		pane.setCaretPosition(doc.getLength());
+		Document doc = pane.getStyledDocument();//获取Document
+		//pane.setCaretPosition(doc.getLength());//设置插入符位置
 		try
 		{
 			doc.insertString(doc.getLength(), text, set);//插入文字
